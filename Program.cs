f84e80ee9e97d7cb0be33d957b745990c075bc95
James Larner <james.larner@aru.ac.uk>
2022-10-05 09:21:19 +0100

    Added code to demonstrate Timer

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+
+
+    Timer _timer = null;
+
+      _timer = new Timer(TimerCallback, null, 0, 2000);
+      // Wait for the user to hit <Enter>
+      Console.ReadLine();
+
+
+   void TimerCallback(Object o) 
+   {
+      // Display the date/time when this method got called.
+      Console.WriteLine("In TimerCallback: " + DateTime.Now);
+   }
